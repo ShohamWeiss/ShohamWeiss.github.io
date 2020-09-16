3e6011076c5d1bfa392ee2e5fb5fb8654f774901
Shoham Weiss <shohamweiss@gmail.com>
2020-09-15 21:19:57 -0500

    Added more projects in portfolio

--- assets/js/isotope-custom.js
+++ assets/js/isotope-custom.js
@@ -1,36 +1,30 @@
 $(document).ready(function() {
-	
-	    
+
+
     /* ======= Isotope plugin ======= */
     /* Ref: http://isotope.metafizzy.co/ */
     // init Isotope    
     var $container = $('.isotope');
-    
-    $container.imagesLoaded(function () {
+
+    $container.imagesLoaded(function() {
         $('.isotope').isotope({
             itemSelector: '.isotope-item',
             layoutMode: 'fitRows',
         });
     });
-    
+
     // filter items on click
-    $('#filters').on( 'click', '.type', function() {
-      var filterValue = $(this).attr('data-filter');
-      $container.isotope({ filter: filterValue });
+    $('#filters').on('click', '.type', function() {
+        var filterValue = $(this).attr('data-filter');
+        $container.isotope({ filter: filterValue });
     });
-    
+
     // change is-checked class on buttons
-    $('.filters').each( function( i, typeGroup ) {
-        var $typeGroup = $( typeGroup );
-        $typeGroup.on( 'click', '.type', function() {
-          $typeGroup.find('.active').removeClass('active');
-          $( this ).addClass('active');
+    $('.filters').each(function(i, typeGroup) {
+        var $typeGroup = $(typeGroup);
+        $typeGroup.on('click', '.type', function() {
+            $typeGroup.find('.active').removeClass('active');
+            $(this).addClass('active');
         });
     });
-
-	  
-    
-
-    
-
 });
