148f67d7577f58084a1ea61081e67fbe14c4615f
Shoham Weiss <shohamweiss@gmail.com>
2020-10-19 20:17:28 -0500

    Added info and reordered

--- script.js
+++ script.js
@@ -27,3 +27,26 @@ $(document).ready(function() {
         });
     });
 });
+
+function fillModal(a) {
+    var title = document.getElementsByClassName('card-title')[a].textContent;
+    var summary = document.getElementsByClassName('card-text')[a].textContent;
+    document.getElementsByClassName('modal-title')[0].innerHTML = title;
+    document.getElementsByClassName('modal-body')[0].innerHTML = summary;
+}
+
+function showAcademic() {
+    document.getElementById("acadbtn").hidden = true;
+    document.getElementById("profbtn").hidden = false;
+    document.getElementById("prof").hidden = true;
+    document.getElementById("acad").hidden = false;
+    document.body.style.background = "#323639";
+}
+
+function showProfessional() {
+    document.getElementById("acadbtn").hidden = false;
+    document.getElementById("profbtn").hidden = true;
+    document.getElementById("prof").hidden = false;
+    document.getElementById("acad").hidden = true;
+    document.body.style.background = "#F7F8FA";
+}
