51a640fbe07cd2781691b20186d8563c703afbf7
shoham <shohamweiss@gmail.com>
2023-01-31 13:45:35 -0600

    adding proj

---
+++ projects/3DHandPoseSelection/options.js
@@ -0,0 +1,47 @@
+options = {
+    "Numbers": {
+        "One": "One",
+        "Two": "Two",
+        "Three": "Three",
+        "Four": "Four",
+        "Five": "Five",
+        "Six": "Six"
+    },
+    "Daily Routines": {
+        "Wake Up":"Wake Up",
+        "Sleep": "Sleep",
+        "Eat": "Eat",
+        "Drink": "Drink",
+        "Shower": "Shower",
+        "School": "School",
+        "Work": "Work",
+    },
+    "Phrases": {
+        "Hello": "Hello"
+    }
+    ,
+    "Words": {
+        "Hello": "Hello"
+    },
+    "Letters": {
+    },
+    "Greetings": {
+        "Hello": "Hello",
+        "Goodbye": "Goodbye",
+        "How are you?": "How are you?",
+        "Have a nice day": "Have a nice day",
+        "Nice to meet you": "Nice to meet you"
+    },
+    "Identifiers": {
+
+    },
+    "Questions": {
+        "What is your name?": "What is your name?",
+        "Who":"Who",
+        "What":"What",
+        "Where":"Where",
+        "When":"When",
+        "Why":"Why",
+        "How":"How"
+    }
+}
